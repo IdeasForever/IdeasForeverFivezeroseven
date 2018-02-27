17126d520c5f52369a2c1b4825beb41bd2ce2427
wangyibo <wangyibo.mail@foxmail.com>
2018-02-27 19:11:54 +0800

    增加状态

---
+++ src/main/java/five/zero/seven/foreveryb/footstone/base/entity/BasicStates.java
@@ -0,0 +1,19 @@
+/**
+ * IdeasForever，2018。
+ * 
+ * 项目名：	IdeasForeverFivezeroseven
+ * 文件名：	BasicStates.java
+ * 模块说明：	
+ * 修改历史：
+ * 2018年2月27日 - wangyibo - 创建。
+ */
+package five.zero.seven.foreveryb.footstone.base.entity;
+
+/**
+ * @author wangyibo
+ *
+ */
+public class BasicStates {
+  public static final String USING = "ineffect";
+  public static final String DISABLED = "disabled";
+}
