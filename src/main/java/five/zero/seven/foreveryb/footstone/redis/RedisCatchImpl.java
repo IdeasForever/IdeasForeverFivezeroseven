7ff836c21916f71f2482f1d8359af43b98488eb6
wangyibo <wangyibo.mail@foxmail.com>
2018-03-16 14:14:38 +0800

    小修改

--- src/main/java/five/zero/seven/foreveryb/footstone/redis/RedisCatchImpl.java
+++ src/main/java/five/zero/seven/foreveryb/footstone/redis/RedisCatchImpl.java
@@ -12,6 +12,7 @@ package five.zero.seven.foreveryb.footstone.redis;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -27,7 +28,7 @@ import five.zero.seven.foreveryb.footstone.util.JsonUtil;
 @Service(value = RedisCatch.DEFAULT_CONTEXT_ID)
 public class RedisCatchImpl implements RedisCatch {
 
-//  @Autowired 此处不用自动注入
+  @Autowired 
   private StringRedisTemplate redisTemplate;
 
   /** redisTemplate： */
